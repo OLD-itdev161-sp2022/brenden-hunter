b7c3a5d9cec5f6190ff3f96cfaceb097d7e9a1b9
brenden hunter <hunterb7@gmatc.matc.edu>
2022-02-11 13:13:30 -0600

    User registration API Endpoint

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-import express from 'express';
+import express, { Router } from 'express';
 import connectDatabase from './config/db';
 
 const app = express();
 
 connectDatabase();
 
+app.unsubscribe(express.json({ extended: false}));
+
+/** 
+*@route GET 
+*@desc   Test endpoint
+*/
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/** 
+*@route POST api/users
+*@desc  register user
+*/
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log(`Express server running on port 3000`));
