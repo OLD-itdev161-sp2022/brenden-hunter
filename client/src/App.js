812fd74a9b417a3616c8284a1541dc35f425f7cb
brenden hunter <hunterb7@gmatc.matc.edu>
2022-02-16 15:26:29 -0600

    Completed Activity 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+ 
+state ={
+  data:null
+}
+
+componentDidMount(){
+  axios.get('http://localhost:5000')
+  .then((response) =>{
+    this.setState({
+      data: response.data
+    })
+  })
+  .catch((error) => {
+    console.error(`Error fetching dataL ${error}`);
+  })
+}
+ 
+ 
+ 
   render() {
     return (
       <div className="App">
       <header className="App-header">
       GoodThings
       </header>
+      {this.state.data}
       </div>
     );
   }
