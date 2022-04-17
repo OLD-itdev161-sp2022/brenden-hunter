c89eb78613770aefd416874778f35712f0eaab22
brenden hunter <hunterb7@gmatc.matc.edu>
2022-04-17 16:18:25 -0500

    Completed Activity 9

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+import mongoose, { trusted } from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+
+    user: {
+        type: 'ObjectId',
+        ref: 'User'
+    },
+    titles: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+export default Post;
